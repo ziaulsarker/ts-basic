8ae4338d2d990b7cf3e40bf3b7b4c9afd67a4cce
ziaul sarker <ziaulsarker@gmail.com>
2022-09-06 22:43:34 -0400

    typeguards -- instanceof guard

--- index.ts
+++ index.ts
@@ -1,7 +1,40 @@
 class Dog {
-  name: string
+  constructor(readonly name: string) {}
+  sayhello = (): string => 'Woof Woof Bitch!!';
 }
 
 class Fish {
-  name: string;
+  constructor(readonly name: string) {}
+  dive = (howDeep: string): string => `Fish Dove ${howDeep} feet`;
 }
+
+type Pet = Dog | Fish;
+
+const d = new Dog("ghost");
+const f = new Fish("shark");
+
+
+
+// type Guards; 
+const isDog = (obj: any): obj is Dog => !!obj && 'sayhello' in obj;
+const isFish = (obj: any): obj is Fish => !!obj && 'dive' in obj;
+
+const talkToPet = (pet: Pet): void => {
+
+  // instanseof Typeguard
+  if (pet instanceof Dog) {
+    return console.log(pet.sayhello());
+  } 
+  
+    // instanseof Typeguard
+  if (pet instanceof Fish) {
+   return console.log(pet.dive("100"));
+  }
+
+  return console.log("thats Typeguard bitch");
+};
+
+talkToPet(d);
+talkToPet(f);
+
+
